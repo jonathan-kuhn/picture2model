36756c28dbd5eb8df5c405def61d2172a4b88367
KSALP; Kuhn Jonathan <jonathan_kuhn@sluz.ch>
2024-03-25 15:07:57 +0100

    added not completely finished code

--- converter.py
+++ converter.py
@@ -1,19 +1,20 @@
 import trimesh
 trimesh.util.attach_to_log()
 height_map = [
-    [0, 0, 0, 0, 0],
+    [0, 3, 0, 2, 0],
     [0, 1, 2, 1, 0],
     [0, 2, 3, 2, 0],
     [0, 1, 2, 1, 0],
     [0, 0, 0, 0, 0]
 ]
+print(len(height_map))
 vertices = []
 print(enumerate(height_map))
 for row, y in enumerate(height_map):
     print(row)
     print(y)
     for col, z in enumerate(y):
-        vertices.append([row, col, z*5])
+        vertices.append([row, col, z])
 
 faces = []
 for row in range(len(height_map) - 1):
@@ -32,3 +33,23 @@ mesh.export('model.stl')
 mesh = trimesh.Trimesh(vertices=[[0, 0, 0], [0, 0, 1], [0, 1, 0], [0, 1, 1]],
                        faces=[[0, 1, 2], [1, 2, 3]])
 mesh.export('model2.stl')
+
+
+
+vertices = []
+faces = []
+for x in range(len(height_map)):
+    for y in range(len(height_map[x])):
+        vertices.append([x, y, height_map[x][y]])
+
+counter = 0
+for x in range(len(height_map)-1):
+    for y in range(len(height_map[x])-1):
+        faces.append([counter, counter+1, counter+len(height_map[x])+1])
+        faces.append([counter, counter+len(height_map[x]), counter+len(height_map[x])+1])
+        counter += 1
+faces.append([0, len(height_map)-1, counter-len())
+faces.append([0, len(height_map)-1, counter]-1)
+
+mesh = trimesh.Trimesh(vertices=vertices, faces=faces)
+mesh.export('model3.stl')        
