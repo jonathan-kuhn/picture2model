d19adb63095c5fa4106f50fef9f612d861d05ebc
Jonathan Kuhn <jonathan.simon.kuhn@gmail.com>
2024-05-30 09:15:25 +0200

    good performance now, basically finished

--- converter.py
+++ converter.py
@@ -1,10 +1,11 @@
 from PIL import Image
 import numpy as np
 from time import sleep, perf_counter
+from sys import exit
 #image = Image.open("Green-Wallpaper-36-640x360.jpg")  # the image to use
 starttime = perf_counter()
 
-filename = "Hannah(2).jpeg"
+filename = "Tree.jpeg"
 image = Image.open(filename)
 greyscale = Image.open(filename).convert('L')
 img_width = image.size[0]  # the width and height of the input image
@@ -15,14 +16,14 @@ print()
 for pix_x in range(img_width):
     row = [0]  # Start with a 0 for the left edge
     for pix_y in range(img_height):
-            luminance = 0.21 * image.getpixel((pix_x, pix_y))[0] + 0.72 * image.getpixel((pix_x, pix_y))[1] + 0.07 * image.getpixel((pix_x, pix_y))[2]
-            brightness = greyscale.getpixel((pix_x, pix_y))
-            if brightness>100:
-                 row.append(2)
-            elif brightness>3:
-                 row.append(3)
-            else:
-                 row.append(4)
+        luminance = 0.21 * image.getpixel((pix_x, pix_y))[0] + 0.72 * image.getpixel((pix_x, pix_y))[1] + 0.07 * image.getpixel((pix_x, pix_y))[2]
+        brightness = greyscale.getpixel((pix_x, pix_y))
+        if brightness>100:
+                row.append(2)
+        elif brightness>3:
+                row.append(3)
+        else:
+                row.append(4)
     row.append(0)  # Add 0 for the right edge
     height_map.append(row)
 
@@ -42,11 +43,18 @@ base_thickness = 1
 vertecies = []
 faces = []
 
+counter = 0
 for x in range(len(height_map)):
+    print(f'x is {x}, len(height_map) is {len(height_map)}')
     print(f'Progress with adding vertecies: {x*100//len(height_map)}%')
     for y in range(len(height_map[x])):
-        vertecies.append([x, y, height_map[x][y]])
+        vertecies.append([x, y, height_map[x][y]]) 
+        if (x != len(height_map)-1) and (y != len(height_map[x])-1):
+            faces.append([counter, counter+1, counter+len(height_map[x])+1])
+            faces.append([counter, counter+len(height_map[y]), counter+len(height_map[x])+1])
+            print(counter)
 
+        counter += 1 
 # base_width = len(height_map[0])   #define the size of the base
 # base_length = len(height_map)
 # vertecies.append([0,0 -base_thickness])   #add four vertecies for the base
@@ -54,19 +62,8 @@ for x in range(len(height_map)):
 # vertecies.append([len(height_map)-1,0,-base_thickness])
 # vertecies.append([len(height_map)-1,len(height_map[0])-1,-base_thickness])
 
-def add_Faces(a):
-    for b in range(len(height_map[a])-1):
-        counter = vertecies.index([a, b, height_map[a][b]])
-        faces.append([counter, counter+1, counter+len(height_map[a])+1])
-        faces.append([counter, counter+len(height_map[a]), counter+len(height_map[x])+1])
-        
-for a in range(len(height_map)-1):
-    print(f'Progress with adding faces: {a*100//(len(height_map)-1)}%')
-    add_Faces(a)
-
-
-faces.append([0, len(height_map[a])-1, len(vertecies)-1])
-faces.append([0, len(vertecies)-(len(height_map[a])), len(vertecies)-1])
+faces.append([0, len(height_map[x])-1, len(vertecies)-1])
+faces.append([0, len(vertecies)-(len(height_map[x])), len(vertecies)-1])
 
 
 
